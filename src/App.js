e55759e3379eb881d4acdcfbf23a83fb81ce1208
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-07-08 22:48:46 -0500

    Add main components and cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-
+import styles from './App.module.css';
 import React from 'react'
+import {Cards} from './components/Cards/Cards'
+import {Chars} from './components/Chars/Chars'
+import {CountryPicker} from './components/CountryPicker/CountryPicker'
+import {fetchData} from './api/index'
 
-export const App = () => {
+class App extends React.Component {
+  state = {
+    data: {},
+  }
   
+ async componentDidMount () {
+    const fetch = await fetchData()
+
+    this.setState({data: fetch})
+  }
+
+  render(){ 
+  const { data } = this.state 
+    
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <Cards  data={data}/>
+      <Chars />
+      <CountryPicker />
     </div>
   )
+ }
 }
+
+export default App
---
+++ src/components/Chars/Chars.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Chars = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
